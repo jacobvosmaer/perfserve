6c8ba177492081e1dafeaefe60f22bef203330c3
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-02-13 17:57:33 +0100

    Fix strange high sample counts in flame graph
    Seems to have be because of this bug:
    https://github.com/brendangregg/FlameGraph/issues/165. One workaround
    there was to record data with `-e cpu-clock`. I don't know what that
    means but that is what this commit does.

--- recordflame.sh
+++ recordflame.sh
@@ -4,5 +4,5 @@ set -e
 freq=99
 sleep=30
 
-perf record -o perf.data -a -g -F $freq -- sleep $sleep
+perf record -o perf.data -a -g -F $freq -e cpu-clock -- sleep $sleep
 perf script -i perf.data | ./stackcollapse-perf.pl | ./flamegraph.pl --title "$1,freq=$freq,sleep=$sleep" --hash
