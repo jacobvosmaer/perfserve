2efaee9de6aed199eaddb9037936354f4c4b4b53
Jacob Vosmaer <contact@jacobvosmaer.nl>
2024-02-13 18:09:01 +0100

    Reformat script

--- recordflame.sh
+++ recordflame.sh
@@ -3,6 +3,7 @@ set -e
 
 freq=${2:-99}
 sleep=${3:-30}
+title="$1,freq=$freq,sleep=$sleep"
 
 perf record -o perf.data -a -g -F $freq -e cpu-clock -- sleep $sleep
-perf script -i perf.data | ./stackcollapse-perf.pl | ./flamegraph.pl --title "$1,freq=$freq,sleep=$sleep" --hash
+perf script -i perf.data | ./stackcollapse-perf.pl | ./flamegraph.pl --title "$title" --hash
